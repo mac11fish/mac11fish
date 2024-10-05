246e8e705ca3d6f31a51127b7dd90075dfb6abf7
coco <cocoboy1337@gmail.com>
2024-10-06 01:35:56 +0200

    simplify homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        mac11fish
-      </main>
+    
+    <main className="">
+    
+
+ <div className="max-w-[600px] mx-auto px-6">
+
+
+  
      
-    </div>
+       <div className="text-xs leading-loose border-t border-current pt-6 pb-40">
+         <h2 className="pb-4">Haftungsauschluss</h2>
+         <p className="leading-relaxed">
+         Die Informationen auf diesen Internetseiten wurden nach bestem
+         Wissen zusammengestellt. Sie dienen der allgemeinen Information. Die
+         hier gegebenen Informationen stellen keine rechtliche Beratung dar.
+         Eine Haftung aufgrund der hier gegebenen allgemeinen Hinweise ist
+         daher ausgeschlossen. Als Service für den Benutzer enthalten diese
+         Seiten außerdem Verweise (Links) zu Internetseiten anderer Anbieter.
+         Auf die Gestaltung und die Inhalte der gelinkten Seiten habe ich
+         keinen Einfluss. Ich übernehme keinerlei Gewähr oder Haftung für den
+         Inhalt, die Aktualität, Korrektheit, Vollständigkeit, Qualität oder
+         Quelle der auf diesen Seiten dargebotenen Informationen oder davon
+         heruntergeladener Dateien. Ich bin in keiner Weise verantwortlich
+         für die Internetseiten Dritter und distanziere mich von deren
+         Inhalten ausdrücklich.
+           </p>
+       </div>
+     </div>
+
+ </main>
+
   );
 }
