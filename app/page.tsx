505dd72df60cc0bde87bc263a9a79dc62743b4dd
coco <cocoboy1337@gmail.com>
2024-10-06 01:21:18 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+<>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         mac11fish
       </main>
-     
+  </>
+    
   );
 }
