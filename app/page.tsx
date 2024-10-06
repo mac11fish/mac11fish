3b03b8e1217d6fd79c06fcd04eb5da143fe916ee
coco <cocoboy1337@gmail.com>
2024-10-06 02:37:41 +0200

    add Degular font

--- app/page.tsx
+++ app/page.tsx
@@ -2,35 +2,15 @@
 export default function Home() {
   return (
     
-    <main className="">
+  <main className="flex h-screen">
     
-
- <div className="max-w-[600px] mx-auto px-6">
-
-
+  <div className="max-w-[600px] px-6 m-auto">
   
-     
-       <div className="text-xs leading-loose border-t border-current pt-6 pb-40">
-         <h2 className="pb-4">Haftungsauschluss</h2>
-         <p className="leading-relaxed">
-         Die Informationen auf diesen Internetseiten wurden nach bestem
-         Wissen zusammengestellt. Sie dienen der allgemeinen Information. Die
-         hier gegebenen Informationen stellen keine rechtliche Beratung dar.
-         Eine Haftung aufgrund der hier gegebenen allgemeinen Hinweise ist
-         daher ausgeschlossen. Als Service für den Benutzer enthalten diese
-         Seiten außerdem Verweise (Links) zu Internetseiten anderer Anbieter.
-         Auf die Gestaltung und die Inhalte der gelinkten Seiten habe ich
-         keinen Einfluss. Ich übernehme keinerlei Gewähr oder Haftung für den
-         Inhalt, die Aktualität, Korrektheit, Vollständigkeit, Qualität oder
-         Quelle der auf diesen Seiten dargebotenen Informationen oder davon
-         heruntergeladener Dateien. Ich bin in keiner Weise verantwortlich
-         für die Internetseiten Dritter und distanziere mich von deren
-         Inhalten ausdrücklich.
-           </p>
-       </div>
+     <div className="text-xl leading-loose -mt-20">
+       mac11fish
      </div>
+   </div>
 
- </main>
-
+</main> 
   );
 }
