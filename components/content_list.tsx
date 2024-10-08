153bafe86ab38d028491f963cff051e7a50bb5da
coco <cocoboy1337@gmail.com>
2024-10-08 22:19:06 +0200

    add new list file, replace carousel

---
+++ components/content_list.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import * as React from "react"
+
+import projectsData from '@/data/projectsData'
+import Image from "next/image"
+
+
+
+export function ContentList() {
+
+  return (
+    <div className="w-full max-w-7xl h-full"
+    >
+      <ul className="-mt-1 h-screen">
+
+
+{projectsData.map((d) => (
+              <li key={d.imgSrc} className="pt-1 md:flex ">
+                <Image
+                className="object-cover w-full md:w-3/4 md:pr-4"
+                aria-hidden
+                src={d.imgSrc as string}
+                alt={d.description}
+                width={900}
+                height={900}
+              />
+
+                <div className=" w-full md:w-1/4 md:pt-24 text-xs ">
+              <h2 className="">{d.title}</h2>
+<p className="">{d.description}</p>
+                </div>
+              </li>
+
+        ))}
+
+
+      </ul>
+
+
+    </div>
+  )
+}
+
