1e5330f3237b3a76378ed50f266136ecc76e4ece
coco <cocoboy1337@gmail.com>
2024-10-08 22:54:23 +0200

    move logo to the bottom

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { ContentList } from '@/components/content_list'
 export default function Home() {
   return (
     <>
-    <div className="h-3 w-60 fixed top-6 left-6 z-[100]">
+    <div className="h-3 fixed bottom-6 right-6 z-[100]">
 
 <Mac11fishLogo />
 </div>
