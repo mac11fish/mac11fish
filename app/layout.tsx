d5dfb609f718d72806d22129d01775c36a355bae
coco <cocoboy1337@gmail.com>
2024-10-08 03:51:17 +0200

    remove unused fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const degularVariable = localFont({
-  src: "./fonts/ohno_degular/DegularVariable.woff2",
-  variable: "--font-degular-sans",
-  weight: "100 900",
-});
-const degularMono = localFont({
-  src: "./fonts/ohno_degular/DegularMono-Regular.woff2",
-  variable: "--font-degular-mono",
-  weight: "100 900",
-});
-
 export const metadata: Metadata = {
   title: "mac11fish",
   description: "..",
