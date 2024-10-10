c1b6f27a9df3d9c58934ff76b92ce371439908fb
coco <cocoboy1337@gmail.com>
2024-10-10 09:30:24 +0200

    add file

---
+++ data/projectsDatax.ts
@@ -0,0 +1,42 @@
+interface Project {
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
+  imgArray?: array
+}
+
+const projectsData: Project[] = [
+  {
+    title: 'Browsers and Client-side Web Technologies',
+    description: `What if you could look up any information in the world? Webpages, images, videos
+    and more. Google has many features to help you find exactly what you're looking
+    for.`,
+    imgSrc: '/images/drawing_5.png',
+    imgArray: ['/images/drawing_5.png','/images/drawing_5.png','/images/drawing_5.png','/images/drawing_5.png',],
+    href: 'https://www.google.com',
+  },
+  {
+    title: 'Introducing the Servo Collective',
+    description: `“If we can make the web platform more modular and easily reusable in both familiar and novel ways, and help build web-platform-grade libraries in underlying areas like networking, graphics, and typography, we could really change the Rust ecosystem. But doing those things well does take time and money, and we can only achieve Servo’s full potential with your help.”`,
+    imgSrc: '/images/drawing_5A.png',
+imgArray: ['/images/drawing_5.png','/images/drawing_5.png','/images/drawing_5.png','/images/drawing_5.png',],
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'The high performance WebKit port for embedded',
+    description: `WPE WebKit is being adopted in a wide range of industries, from cable operators to consumer electronic device manufacturers, with tens of millions of devices using it worldwide. Igalia can help you get the maximum performance out of your hardware by optimizing WPE WebKit for your needs.`,
+    imgSrc: '/images/drawing_5E.png',
+imgArray: ['/images/drawing_5.png','/images/drawing_5.png','/images/drawing_5.png','/images/drawing_5.png',],
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'GStreamer development',
+    description: `We have a strong multimedia team with many experienced GStreamer developers. GStreamer hosts many of our contributions to different layers of the stack from the kernel up to the end-user application.`,
+    imgSrc: '/images/zombie_felon.png',
+imgArray: ['/images/drawing_5.png','/images/drawing_5.png','/images/drawing_5.png','/images/drawing_5.png',],
+    href: '/blog/the-time-machine',
+  },
+]
+
+export default projectsData
