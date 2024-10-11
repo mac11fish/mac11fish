ab047015e49c67b4f0485a53438d8a9f49e7c847
coco <cocoboy1337@gmail.com>
2024-10-11 22:50:08 +0200

    many edits, restarted from scratch after contentlayer install

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,25 @@
+import '../styles/globals.css'
+import { ProgressLine } from '../components/progress_line'
+import { Mac11fishLogo } from '../components/mac11fish_logo'
 import type { Metadata } from "next";
-import "./globals.css";
+
 
 export const metadata: Metadata = {
   title: "mac11fish",
   description: "..",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body
-        className={``}
-      >
-        {children}
+    <html>
+
+      <body>
+        <Mac11fishLogo />
+        <ProgressLine />
+
+        <div className="">{children}</div>
       </body>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,13 @@
-
-
-import { Mac11fishLogo } from '@/components/Mac11fishLogo'
-import { ContentList } from '@/components/content_list'
+import Link from "next/link";
+import { compareDesc, format, parseISO } from "date-fns";
+import { allPosts, Post } from "contentlayer/generated";
+import { getMDXComponent } from "next-contentlayer/hooks";
+import { PostList } from '../components/post_list'
 
 
 export default function Home() {
+
   return (
-    <>
-    <div className="h-3 fixed bottom-6 right-6 z-[100]">
-
-<Mac11fishLogo />
-</div>
-
-  <main className="w-full h-screen mx-auto">
-
-
-<ContentList/>
-
-
-</main> 
-</>
-
+    <PostList />
   );
 }
