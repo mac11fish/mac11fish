5c82616734b322777a315bf194124f35a46a44b0
coco <cocoboy1337@gmail.com>
2024-10-15 09:59:04 +0200

    x

---
+++ components/ScrollDown.tsx
@@ -0,0 +1,32 @@
+
+"use client"
+
+import { motion, useScroll, useSpring } from "framer-motion";
+
+
+export function ScrollDown() {
+  const { scrollYProgress } = useScroll()
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+  const scaleXb = 1 - scrollYProgress
+
+
+  return (
+<>
+<motion.div  className="p-6 flex" >
+       <div> scroll down</div>
+
+<div  className="pl-1 animate-[bounce_0.75s_ease-in-out_infinite]">
+<svg className="w-4 h-4 text-current" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+      <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+    </svg>
+</div>
+</motion.div>
+
+  </>  
+  )
+}
+
